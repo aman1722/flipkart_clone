6e45a793a844404336f5ba96cef4eadb3083135a
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-06-28 21:43:11 +0530

    home page sliders

--- frontend/src/components/home/Slide.jsx
+++ frontend/src/components/home/Slide.jsx
@@ -1,7 +1,7 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
-import { Box, Typography , styled } from "@mui/material";
+import { Box, Typography , Button , styled } from "@mui/material";
 import Countdown from 'react-countdown';
 
 
@@ -27,18 +27,44 @@ const Componant = styled(Typography)`
 `
 const Deal = styled(Typography)`
   padding: 15px 20px;
-  
+  display: flex;
+
 `
+
+const Timer = styled(Box)`
+  display: flex;
+  margin-left: 10px;
+  align-items:center;
+  color: #7f7f7f;
+`
+
+const DaelText=styled(Typography)`
+  font-size:22px;
+  font-weight:600;
+  margin-right: 25px;
+  line-height: 32px;
+
+`
+const ViewAllButton = styled(Button)`
+  margin-left : auto;
+`
+
 const Slide = ({ products }) => {
   const timerURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/timer_a73398.svg';
+
+  const renderer = ({ hours,minutes,seconds })=>{
+        return <Box variant="span">{hours} : {minutes} : {seconds} Left</Box>
+  }
+
   return (
     <Componant>
       <Deal>
-        <Typography>Deal of The Day</Typography>
-        <Box>
-          <img src={timerURL} alt="timer" />
-          <Countdown date={Date.now() + 5.04e+7} />
-        </Box>
+        <DaelText>Deal of The Day</DaelText>
+        <Timer>
+          <img src={timerURL} alt="timer" style={{ width: 24 }}/>
+          <Countdown date={Date.now() + 5.04e+7} renderer={renderer} />
+        </Timer>
+        <ViewAllButton variant="contained">View All</ViewAllButton>
       </Deal>
       <Carousel
         responsive={responsive}
